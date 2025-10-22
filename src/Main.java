5331b1f3a53525ad089e5975d67dd1138b771975
Sheyla Ponce <sheylaponce11@gmail.com>
2025-10-22 18:02:29 +0200

    update

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    // esto es un comentario
+    /*
+    este comentario
+    se puede dividir en
+    varias líneas de código
+    */
+
+    // TODO me falta esto
+
+    public static void main(String[] args) {
+        System.out.println("Esta es la segunda línea del programa\n");
+        System.out.println("Este es mi primer programa en Java\n");
+    }
+
+    public void nombreMetodo() {
+    }
+
+}
