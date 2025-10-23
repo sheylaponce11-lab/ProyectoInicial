8d13ce9f770949b8d8a72b2f95b828094cba286d
Sheyla Ponce <sheylaponce11@gmail.com>
2025-10-23 12:48:45 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,30 @@
 public class Main {
-    // esto es un comentario
-    /*
-    este comentario
-    se puede dividir en
-    varias líneas de código
-    */
-
-    // TODO me falta esto
-
+    //El nombre de la clase la primera siempre en mayuscula
+    //esto es un comentario
     public static void main(String[] args) {
         System.out.println("Esta es la segunda línea del programa\n");
         System.out.println("Este es mi primer programa en Java\n");
 
     }
+    /*
+    Si a un valor le ponemos final delante no se puede modificar luego.
+    */
+    final String DNI = "123123A";
+    // TODO me falta esto
+
+    int edadGlobal = 12;
+
+    public static void main(String[] args)
+
+    public void nombreMetodo()
+
+    public void nombreMetodo1(int edadGlobal){
+        int edadMetodo = 123;
+        System.out.println(edadMetodo);
+        System.out.println(this.edadGlobal);
+        //Para llamar a una variable definida fuera, se le pone this.
+    }
+
     //El /n es para dejar una linea y sigue en la siguiente.
     public void nombreMetodo() {
     }
