de252cf87232f11c9a35436e44698ccde8be38c4
Sheyla Ponce <sheylaponce11@gmail.com>
2025-10-23 13:22:52 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,33 @@
 public class Main {
-    //El nombre de la clase la primera siempre en mayuscula
-    //esto es un comentario
+    // El nombre de la clase la primera siempre en mayúscula
+    // Esto es un comentario
+
+    /*
+    Si a un valor le ponemos 'final' delante no se puede modificar luego.
+    */
+    final static String DNI = "123123A";
+    // TODO me falta esto
+
+    static int edadGlobal = 12;
+
     public static void main(String[] args) {
         System.out.println("Esta es la segunda línea del programa\n");
         System.out.println("Este es mi primer programa en Java\n");
 
+        // Creamos un objeto para poder llamar a los métodos no estáticos
+        Main objeto = new Main();
+        objeto.nombreMetodo1(edadGlobal);
     }
-    /*
-    Si a un valor le ponemos final delante no se puede modificar luego.
-    */
-    final String DNI = "123123A";
-    // TODO me falta esto
 
-    int edadGlobal = 12;
+    public void nombreMetodo() {
+        System.out.println("Has llamado a nombreMetodo()");
+    }
 
-    public static void main(String[] args)
-
-    public void nombreMetodo()
-
-    public void nombreMetodo1(int edadGlobal){
+    public void nombreMetodo1(int edadGlobal) {
         int edadMetodo = 123;
         System.out.println(edadMetodo);
         System.out.println(this.edadGlobal);
-        //Para llamar a una variable definida fuera, se le pone this.
+        // Para llamar a una variable definida fuera, se le pone this.
     }
-
-    //El /n es para dejar una linea y sigue en la siguiente.
-    public void nombreMetodo() {
-    }
-
 }
+
--- src/MainMetodos.java
+++ src/MainMetodos.java
@@ -1,2 +1,15 @@
 public class MainMetodos {
+    public static void main(String[] args) {
+        System.out.println("Main para probar métodos y variables");
+        String nombre = "Borja";
+        saludar("Borja");
+        saludar("Laura");
+
+        //saludar de arriba es estático, para ejecutarlo todo tiene que ser estático, por eso luego tiene que ser public STATIC void
+    }
+
+    public static void saludar(String data){
+        System.out.println("Enhorabuena " +data+" has llamado a tu primer método");
+        // IMPORTANTE dejar espacios en la frase entre comillas para que no aparezca junto
+    }
 }
